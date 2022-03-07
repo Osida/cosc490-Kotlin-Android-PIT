e5f52ba9091d2e731a2f55cd056a8a1a168feb8e
Osida Richards <osida.richards@gmail.com>
2022-03-07 11:36:02 -0500

    first commit

---
+++ app/src/test/java/code/with/cal/kotlincalculatorapp/ExampleUnitTest.kt
@@ -0,0 +1,33 @@
+package code.with.cal.kotlincalculatorapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun subtraction_isCorrect() {
+        assertEquals(0, 2 - 2)
+    }
+
+    @Test
+    fun multiplication_isCorrect() {
+        assertEquals(18, 2 * 9)
+    }
+
+    @Test
+    fun division__isCorrect() {
+        assertEquals(6, 30 / 5)
+    }
+
+}
